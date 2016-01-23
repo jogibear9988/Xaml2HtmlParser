8192dae79b09ad53ce1c3ccd9f67680c2c96e792
jogibear9988 <jochen.kuehner@gmx.de>
2016-01-23 13:13:24 +0100

    Initial Application Platform for Xaml2HtmlParser

---
+++ Xaml2HtmlParser/Xaml2HtmlParser/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Xaml2HtmlParser
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
